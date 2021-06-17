d8c820d87b7fc8da4359647e246417dd4bbb93d4
Andreas Heim <dinkumoil@users.noreply.github.com>
2021-06-17 22:01:47 +0200

    ExplorerContextMenu: - changed: Improved install scripts - changed: Removed dependency from ICO file for OpenConsoleHere - changed: Added info file for HStart.exe to OpenConsoleHere

--- ExplorerContextMenu/OpenConsoleHere/install.cmd
+++ ExplorerContextMenu/OpenConsoleHere/install.cmd
@@ -1,41 +1,82 @@
 @echo off & setlocal
 
-set "DestFolder=C:\Windows\system32"
-set "FilesToCopy="AdminCMD.ico""
+::------------------------------------------------------------------------------
+:: Basic configuration
+::------------------------------------------------------------------------------
+set "DestFolder="
+set "FilesToCopy="
 set "RegFileToExecute=OpenConsole.reg"
 
+::Path for temporary elevation script
+set "ElevateScript=%Temp%\Elevate.vbs"
+
+
+::------------------------------------------------------------------------------
+:: Startup sequence
+::------------------------------------------------------------------------------
+::Set working directory to script's path
 pushd "%~dp0"
 
-set "VBScript=%TEMP%\Elevate.vbs"
-call :WriteVBScript "%CD%" "%~nx0"
-
-if /i "%~1" neq "/elevated" (
-  start "" wscript /nologo "%VBScript%"
-  exit /b 0
+::Check for admin permissions and restart elevated if required
+call :CheckForAdminPermissions || (
+  call :RestartElevated "%~f0"
+  goto :Terminate
 )
 
-md "%DestFolder%" 2>NUL
 
-for %%a in (%FilesToCopy%) do (
-  copy "%%~a" "%DestFolder%" > NUL
+::------------------------------------------------------------------------------
+:: Perform installation
+::------------------------------------------------------------------------------
+if defined DestFolder (
+  md "%DestFolder%" 2>NUL
+
+  for %%a in (%FilesToCopy%) do (
+    copy "%%~a" "%DestFolder%" > NUL
+  )
 )
 
 start "" regedit /s "%RegFileToExecute%"
 
-del "%VBScript%" > NUL
+::Clean up
+del "%ElevateScript%" 1>NUL 2>&1
 
+
+::------------------------------------------------------------------------------
+:: Script termination
+::------------------------------------------------------------------------------
+:Terminate
 popd
-exit /b
+exit /b 0
 
 
-:WriteVBScript
-  chcp 1252 > NUL
-  > "%VBScript%" echo.Set objShell = CreateObject("Shell.Application")
-  >>"%VBScript%" echo.Set objFSO   = CreateObject("Scripting.FileSystemObject")
-  >>"%VBScript%" echo.
-  >>"%VBScript%" echo.strApplication = "cmd.exe"
-  >>"%VBScript%" echo.strArguments   = "/c """"" ^& objFSO.BuildPath("%~1", "%~2") ^& """ /elevated"""
-  >>"%VBScript%" echo.
-  >>"%VBScript%" echo.objShell.ShellExecute strApplication, strArguments, "", "runas", 1
-  chcp 850 > NUL
+
+::==============================================================================
+:: Subroutines
+::==============================================================================
+
+:CheckForAdminPermissions
+  net session 1>NUL 2>&1
+  if ERRORLEVEL 1 exit /b 1
+exit /b 0
+
+
+:RestartElevated
+  ::Get system's ANSI and OEM code page and set console's code page to ANSI code page.
+  ::This is required if this script is stored in a path that contains characters
+  ::with different code points in those code pages.
+  for /f "tokens=2 delims==" %%a in ('wmic OS get CodeSet /format:list') do set /a "ACP=%%~a"
+  for /f "tokens=2 delims=.:" %%a in ('chcp') do set /a "OEMCP=%%a"
+  if "%ACP%" neq "" if "%ACP%" neq "0" chcp %ACP% > NUL
+
+  > "%ElevateScript%" echo.Set objShell = CreateObject("Shell.Application")
+  >>"%ElevateScript%" echo.
+  >>"%ElevateScript%" echo.strApplication = "cmd.exe"
+  >>"%ElevateScript%" echo.strArguments   = "/c ""%~1"""
+  >>"%ElevateScript%" echo.
+  >>"%ElevateScript%" echo.objShell.ShellExecute strApplication, strArguments, "", "runas", 1
+
+  ::Restore OEM code page
+  if "%OEMCP%" neq "" if "%OEMCP%" neq "0" chcp %OEMCP% > NUL
+
+  cscript /nologo "%ElevateScript%"
 exit /b 0
